6a336860f3adf6af0615dc834c44bfc9ef651470
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2015-01-22 13:12:20 -0500

    added comment to member functions (Methods)

--- F/02-Jan22/container.h
+++ F/02-Jan22/container.h
@@ -9,9 +9,12 @@ namespace oop244f{
     char _contentType[41];
     char _unit[41];
   public:
-    void setContentType(const char contentType[]);
-    const char* getContentType();
-    double fill(double quantity);
+    void setContentType(const char contentType[]);  // sets the type of conent a container can hold
+    const char* getContentType();                   // gets and returns the conent type to the caller progeram
+    double fill(double quantity);        // fill the container with "quantity" amount of "_contentType",
+                                         // if the quantity overflows the "_capacity", it will return
+                                         // the spillage amount, otherwise zero, (obviously)
+
   };
 
 
