2e26acaeaff48d59b70f21880583dd645f7d321b
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2015-02-03 16:19:15 -0500

    Feb 03, cin / cout section F

---
+++ F/05-Feb03/01-leadingAndTrailingSpaces.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  char str[21];
+
+  cout << "Enter a string : " << endl;
+  cin >> str;
+  cout << "|" << str << "|" << endl;
+}
+
+// enter a word with spaces before and after. 
